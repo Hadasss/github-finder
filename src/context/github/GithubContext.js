3dc1397d7f6f3c9221c47410ed08a4e1662d1abc
Hadasss <hadas.gadish@gmail.com>
2022-10-18 12:59:23 -0700

    added user repos to single user display

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   };
 
@@ -21,6 +23,7 @@ export const GithubProvider = ({ children }) => {
     const params = new URLSearchParams({
       q: text,
     });
+
     const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: { Authorization: `token ${GITHUB_TOKEN}` },
     });
@@ -29,6 +32,26 @@ export const GithubProvider = ({ children }) => {
       type: "GET_USERS",
       payload: items,
     });
+  };
+
+  // GET single user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: { Authorization: `token ${GITHUB_TOKEN}` },
+    });
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
   };
 
   // set loading
@@ -45,13 +68,40 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  // GET user repos
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: { Authorization: `token ${GITHUB_TOKEN}` },
+      }
+    );
+
+    const data = await response.json();
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
+  };
+
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
+        repos: state.repos,
         searchUsers,
         clearResults,
+        getUser,
+        getUserRepos,
       }}
     >
       {children}
